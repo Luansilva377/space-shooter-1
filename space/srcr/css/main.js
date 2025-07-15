2f88f32bac3b38cd0db6ba590ab50b97bf40008d
Luan Silva <152622527+Luansilva377@users.noreply.github.com>
2025-07-15 20:20:57 -0300

    Add files via upload

---
+++ space/srcr/css/main.js
@@ -0,0 +1,7 @@
+
+var canvas = document.querySelector("canvas");
+var ctx = document.querySelector("canvas");
+ctx = c.getContext("2d");
+canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
